7b1c76b95637ff486a49e0d73e958388d9b90a6e
helin <lin.he@sysbox.com.au>
2023-05-24 10:56:59 +1000

    master: added test dashboard.

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('mgg-root')
-);
+
+const appRoot = document.getElementById('mgg-root');
+if (appRoot) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    appRoot
+  );
+}
+
+const roots = document.querySelectorAll('.mgg-app.test') || [];
+for (let i = 0; i < roots.length; i++) {
+  ReactDOM.render(
+    // @ts-ignore
+    <React.StrictMode><h3>test</h3></React.StrictMode>,
+    roots[i]
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
