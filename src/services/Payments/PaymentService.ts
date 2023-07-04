14b2fbb6806a461bfa1724742277452d31375b24
helin <lin.he@sysbox.com.au>
2023-07-04 16:12:04 +1000

    yarn-upgrade/2023-06-02: upgraded packages.

--- src/services/Payments/PaymentService.ts
+++ src/services/Payments/PaymentService.ts
@@ -8,7 +8,7 @@ const getWestpacSettings = (params?: iConfigParams, options?: iConfigParams): Pr
 
 
 const makeADonation = (data: iConfigParams, options?: iConfigParams): Promise<{success: boolean, message: string}> => {
-  return AppService.get(`${endPoint}/donation`, data, options).then(resp => resp.data);
+  return AppService.post(`${endPoint}/donation`, data, options).then(resp => resp.data);
 }
 const PaymentService = {
   getWestpacSettings,
