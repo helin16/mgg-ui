7685c246c3d7d04e368c484c0281288b6ecbd248
helin <lin.he@sysbox.com.au>
2023-07-04 14:54:56 +1000

    yarn-upgrade/2023-06-02: upgraded packages.

--- src/LoadComponents.tsx
+++ src/LoadComponents.tsx
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom";
 import React, {ReactComponentElement} from "react";
+import OnlineDonation from './pages/OnlineDonation/OnlineDonation';
 
 const load = (query: string, component: ReactComponentElement<any>) => {
   const roots = document.querySelectorAll(query) || [];
+  console.log('roots', roots);
   for (let i = 0; i < roots.length; i++) {
     ReactDOM.render(component, roots[i]);
   }
@@ -10,10 +12,10 @@ const load = (query: string, component: ReactComponentElement<any>) => {
 
 const loadAll = () => {
   load(
-    '.mgg-app.test-div',
+    '.mgg-app.online-donation',
     // @ts-ignore
     <React.StrictMode>
-      <div>MGG App Loaded.</div>
+      <OnlineDonation />
     </React.StrictMode>
   );
 }
