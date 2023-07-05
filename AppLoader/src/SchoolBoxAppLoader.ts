b695bfecad3309c7ebe12333742fe69eb2f9741f
helin <lin.he@sysbox.com.au>
2023-07-05 15:06:26 +1000

    master: add appLoader js.

--- AppLoader/src/SchoolBoxAppLoader.ts
+++ AppLoader/src/SchoolBoxAppLoader.ts
@@ -1,36 +1,30 @@
 import AppLoader from './AppLoader';
 
 const SCHOOL_BOX_APP_HTML_ID = 'mgg-root';
-
-
-const checkAndInitHtml = () => {
-  const iFrame = document.querySelector('iframe#remote');
-  if (iFrame) {
-    // @ts-ignore
-    iFrame.style.display = 'none';
-    const reactDiv = document.createElement("div");
-    reactDiv.setAttribute('id', SCHOOL_BOX_APP_HTML_ID);
-    // @ts-ignore
-    reactDiv.setAttribute('data-url', `${iFrame.src || ''}`);
-    if(iFrame.nextSibling){
+export default class SchoolBoxAppLoader {
+  #checkAndInitHtml() {
+    const iFrame = document.querySelector('iframe#remote');
+    if (iFrame) {
       // @ts-ignore
-      iFrame.parentNode.insertBefore(reactDiv, iFrame.nextSibling);
-    } else{
+      iFrame.style.display = 'none';
+      const reactDiv = document.createElement("div");
+      reactDiv.setAttribute('id', SCHOOL_BOX_APP_HTML_ID);
       // @ts-ignore
-      iFrame.parentNode.appendChild(reactDiv);
+      reactDiv.setAttribute('data-url', `${iFrame.src || ''}`);
+      if(iFrame.nextSibling){
+        // @ts-ignore
+        iFrame.parentNode.insertBefore(reactDiv, iFrame.nextSibling);
+      } else{
+        // @ts-ignore
+        iFrame.parentNode.appendChild(reactDiv);
+      }
+      return true;
     }
-    return true;
+    return false;
   }
-  return false;
-}
 
-const init = (appScriptUrl: string) => {
-  const loadCss = checkAndInitHtml();
-  AppLoader.init(appScriptUrl, loadCss, true);
+  init(appScriptUrl: string){
+    const loadCss = this.#checkAndInitHtml();
+    (new AppLoader()).init(appScriptUrl, loadCss, true);
+  }
 }
-
-const SchoolBoxAppLoader = {
-  init
-}
-
-export default SchoolBoxAppLoader;
