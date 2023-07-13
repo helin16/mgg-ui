12433b4291c42da0ca0df7f3fc719370bee428ce
helin <lin.he@sysbox.com.au>
2023-07-13 12:05:20 +1000

    yarn-upgrade/2023-06-02: upgraded packages.

--- src/layouts/SchoolBox/SchoolBoxRouter.tsx
+++ src/layouts/SchoolBox/SchoolBoxRouter.tsx
@@ -97,6 +97,7 @@ const SchoolBoxRouter = ({path, remoteUrl}: {path: string, remoteUrl: string}) =
       removeSchoolBoxIframe();
       return <StudentAbsenceParentSubmissionForm />
     }
+    
     // case '/student_inout/home': {
     case '/student_absence/home': {
       removeSchoolBoxIframe();
