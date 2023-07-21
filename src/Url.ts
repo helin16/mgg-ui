e9836b2c02558e17d12659e6b91820ef88d5fa02
helin <lin.he@sysbox.com.au>
2023-07-21 10:14:56 +1000

    master: added financial figures for forecast.

--- src/Url.ts
+++ src/Url.ts
@@ -1,3 +1,4 @@
 export const URL_ASSET_PICK_UP = '/asset/pickup';
 export const URL_ALUMNI_REGISTRATION = '/community/registration';
 export const URL_ONLINE_DONATION = '/online_donation';
+export const URL_FUNNEL_THANK_YOU_PAGE = '/funnel/thankYou/:pageName';
