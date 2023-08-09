3e0281b1782040685e2827f0ec649ed8e0caeb9d
helin <lin.he@sysbox.com.au>
2023-08-09 10:42:06 +1000

    master: added financial figures for forecast.

--- src/pages/Enrolments/EnrolmentManagementPage.tsx
+++ src/pages/Enrolments/EnrolmentManagementPage.tsx
@@ -8,12 +8,13 @@ import ExpiringPassportsAndVisas from '../../components/Enrolments/ExpiringPassp
 import moment from 'moment-timezone';
 import StudentNumberForecastDashboard
   from '../../components/reports/StudentNumberForecast/StudentNumberForecastDashboard';
+import MathHelper from '../../helper/MathHelper';
 
 const TAB_EXPIRING_PASSPORTS = 'EXPIRING_PASSPORTS';
 const TAB_FORECAST_NEXT_YEAR = 'FORECAST_NEXT_YEAR';
 const EnrolmentManagementPage = () => {
   const defaultSelectedTab = TAB_EXPIRING_PASSPORTS;
-  const [selectedTab, setSelectedTab] = useState(TAB_EXPIRING_PASSPORTS)
+  const [selectedTab, setSelectedTab] = useState(TAB_EXPIRING_PASSPORTS);
   return (
     <Page title={<h3>Enrolments Manager</h3>} AdminPage={EnrolmentManagementAdminPage} moduleId={MGGS_MODULE_ID_ENROLMENTS}>
       <Tabs
@@ -27,7 +28,7 @@ const EnrolmentManagementPage = () => {
           </SectionDiv>
         </Tab>
 
-        <Tab title={`Forecast ${moment().year()}`}  eventKey={TAB_FORECAST_NEXT_YEAR}>
+        <Tab title={`Forecast ${MathHelper.add(moment().year(), 1)}`}  eventKey={TAB_FORECAST_NEXT_YEAR}>
           <SectionDiv>
             <StudentNumberForecastDashboard />
           </SectionDiv>
