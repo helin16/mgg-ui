69771af92ed08a82a546bc5d70f9ca34a95e3921
helin <lin.he@sysbox.com.au>
2023-08-09 10:26:51 +1000

    master: added financial figures for forecast.

--- src/pages/Enrolments/EnrolmentManagementPage.tsx
+++ src/pages/Enrolments/EnrolmentManagementPage.tsx
@@ -5,8 +5,12 @@ import {Tab, Tabs} from 'react-bootstrap';
 import {useState} from 'react';
 import SectionDiv from '../../components/common/SectionDiv';
 import ExpiringPassportsAndVisas from '../../components/Enrolments/ExpiringPassportsAndVisas';
+import moment from 'moment-timezone';
+import StudentNumberForecastDashboard
+  from '../../components/reports/StudentNumberForecast/StudentNumberForecastDashboard';
 
-const TAB_EXPIRING_PASSPORTS = 'Expiring Passports/Visas'
+const TAB_EXPIRING_PASSPORTS = 'EXPIRING_PASSPORTS';
+const TAB_FORECAST_NEXT_YEAR = 'FORECAST_NEXT_YEAR';
 const EnrolmentManagementPage = () => {
   const defaultSelectedTab = TAB_EXPIRING_PASSPORTS;
   const [selectedTab, setSelectedTab] = useState(TAB_EXPIRING_PASSPORTS)
@@ -20,6 +24,12 @@ const EnrolmentManagementPage = () => {
         <Tab title={'Expiring Passports/Visas'}  eventKey={TAB_EXPIRING_PASSPORTS}>
           <SectionDiv>
             <ExpiringPassportsAndVisas />
+          </SectionDiv>
+        </Tab>
+
+        <Tab title={`Forecast ${moment().year()}`}  eventKey={TAB_FORECAST_NEXT_YEAR}>
+          <SectionDiv>
+            <StudentNumberForecastDashboard />
           </SectionDiv>
         </Tab>
       </Tabs>
