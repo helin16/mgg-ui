c0a6b008e23ad3fcf4fa4eec7287df2a5a40ddac
helin <lin.he@sysbox.com.au>
2024-01-19 14:48:14 +1100

    master: added PowerBi FullScreen.

--- src/LoadComponents.tsx
+++ src/LoadComponents.tsx
@@ -3,8 +3,8 @@ import React, {ReactComponentElement} from "react";
 import OnlineDonation from './pages/OnlineDonation/OnlineDonation';
 import {ToastContainer} from 'react-toastify';
 import AlumniRegistrationForm from './pages/alumni/components/AlumniRegistrationForm';
-import {URL_STUDENT_REPORT_SCHOOL_BOX_ID} from './Url';
-import UtilsService from './services/UtilsService';
+// import {URL_STUDENT_REPORT_SCHOOL_BOX_ID} from './Url';
+// import UtilsService from './services/UtilsService';
 
 const load = (query: string, component: ReactComponentElement<any>) => {
   const roots = document.querySelectorAll(query) || [];
@@ -45,17 +45,17 @@ const loadAll = () => {
     getElement(<AlumniRegistrationForm />)
   );
 
-  // Student Academic Report
-  const urlPattern = /\/user\/profile\/documents\/reports\/\d+$/;
-  document.querySelectorAll('a').forEach(element => {
-    const match = `${element.href || ''}`.trim().match(urlPattern);
-    if (`${element.textContent || ''}`.trim().includes('Academic Reports') && match !== null) {
-      const studentSchoolBoxId = match[0];
-      console.log('match1', match);
-      const newUrl = URL_STUDENT_REPORT_SCHOOL_BOX_ID.replace(':schoolBoxId', studentSchoolBoxId);
-      element.href = UtilsService.getModuleUrl(newUrl);
-    }
-  });
+  // // Student Academic Report
+  // const urlPattern = /\/user\/profile\/documents\/reports\/\d+$/;
+  // document.querySelectorAll('a').forEach(element => {
+  //   const match = `${element.href || ''}`.trim().match(urlPattern);
+  //   if (`${element.textContent || ''}`.trim().includes('Academic Reports') && match !== null) {
+  //     const studentSchoolBoxId = match[0];
+  //     console.log('match1', match);
+  //     const newUrl = URL_STUDENT_REPORT_SCHOOL_BOX_ID.replace(':schoolBoxId', studentSchoolBoxId);
+  //     element.href = UtilsService.getModuleUrl(newUrl);
+  //   }
+  // });
 }
 
 const LoadComponents = {
