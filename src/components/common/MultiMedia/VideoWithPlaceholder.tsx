d1e28afcacc341ecfd206c00e4cb7f5cb53a3d70
helin <lin.he@sysbox.com.au>
2024-03-04 11:50:44 +1100

    master: changed script for docker.

--- src/components/common/MultiMedia/VideoWithPlaceholder.tsx
+++ src/components/common/MultiMedia/VideoWithPlaceholder.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import ReactPlayer, { ReactPlayerProps } from "react-player";
 export type VideoPlayer = ReactPlayer;
 
@@ -19,6 +19,45 @@ const VideoWithPlaceholder = ({
   ...props
 }: iVideoWithPlaceholder) => {
   const playerRef = useRef<ReactPlayer>(null);
+  const [scriptUrl, setScriptUrl] = useState('');
+
+  useEffect(() => {
+    const fetchAssetManifest = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_URL || ''}/asset-manifest.json`);
+        if (response.ok) {
+          const data = await response.json();
+          // Assuming the file you need is named reactPlayerFilePlayer.js
+          const scriptUrl = data['reactPlayerFilePlayer.js'];
+          setScriptUrl(scriptUrl);
+        } else {
+          console.error('Failed to fetch asset manifest');
+        }
+      } catch (error) {
+        console.error('Error fetching asset manifest:', error);
+      }
+    };
+
+    fetchAssetManifest();
+
+    return () => {
+      // Cleanup if needed
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scriptUrl) {
+      const script = document.createElement('script');
+      script.src = scriptUrl;
+      script.async = true;
+
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, [scriptUrl]);
 
   const getContent = () => {
     if (!src || src.trim() === "") {
