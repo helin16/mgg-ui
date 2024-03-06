67c3329433f465b0c384e9735c8df72a85a369a8
helin <lin.he@sysbox.com.au>
2024-03-06 11:09:27 +1100

    master: removed junit.

--- src/pages/CampusDisplay/CampusDisplayManagementAdminPage.tsx
+++ src/pages/CampusDisplay/CampusDisplayManagementAdminPage.tsx
@@ -8,8 +8,14 @@ import iCampusDisplay from "../../types/CampusDisplay/iCampusDisplay";
 import PlayListEditPanel from "../../components/CampusDisplay/Playlist/PlayListEditPanel";
 import {Button, Col, Row} from 'react-bootstrap';
 import * as Icons from 'react-bootstrap-icons';
+import {ASSET_TYPE_CAMPUS_DISPLAY} from '../../types/asset/iAsset';
+import AssetListPanel from '../../components/Asset/AssetListPanel';
+import AssetInfoPanel from '../../components/Asset/AssetInfoPanel';
+import * as _ from 'lodash';
 
 const CampusDisplayManagementAdminPage = ({ onNavBack }: AdminPageProps) => {
+  const [selectedAssetIds, setSelectedAssetIds] = useState<string[]>([]);
+  const [viewingAssetId, setViewingAssetId] = useState<string | null>(null);
   const [selectedDisplay, setSelectedDisplay] = useState<iCampusDisplay | null>(
     null
   );
@@ -56,6 +62,47 @@ const CampusDisplayManagementAdminPage = ({ onNavBack }: AdminPageProps) => {
                             size={"sm"}
                             title={"Close"}
                             onClick={() => setSelectedDisplay(null)}
+                          >
+                            <Icons.X />
+                          </Button>
+                        </>
+                      }
+                    />
+                  </Col>
+                )}
+              </Row>
+            )
+          },
+          {
+            key: "assets",
+            title: "Media Library",
+            component: (
+              <Row>
+                <Col lg={`${viewingAssetId || ''}`.trim() !== '' ? 8 : 12}>
+                  <AssetListPanel
+                    allowCreation
+                    allowDeletion={true}
+                    assetType={ASSET_TYPE_CAMPUS_DISPLAY}
+                    selectedAssetIds={selectedAssetIds}
+                    onSelect={newIds => {
+                      const diff = _.difference(newIds, selectedAssetIds);
+                      setViewingAssetId(diff.length > 0 ? diff[0]  : null);
+                      setSelectedAssetIds(newIds);
+                    }}
+                  />
+                </Col>
+                {`${viewingAssetId || ''}`.trim() === '' ? null : (
+                  <Col lg={4}>
+                    <AssetInfoPanel
+                      className={"asset-info"}
+                      assetId={`${viewingAssetId || ''}`.trim()}
+                      extraBtns={
+                        <>
+                          <Button
+                            variant={"secondary"}
+                            size={"sm"}
+                            title={"Close"}
+                            onClick={() => setViewingAssetId(null)}
                           >
                             <Icons.X />
                           </Button>
