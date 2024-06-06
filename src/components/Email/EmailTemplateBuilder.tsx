5d622a144c7d7057587a23208ade52fc86ce7c0c
Lin He <showmine.sysbox@gmail.com>
2024-06-06 15:43:54 +1000

    master: added email builder.

--- src/components/Email/EmailTemplateBuilder.tsx
+++ src/components/Email/EmailTemplateBuilder.tsx
@@ -1,30 +1,49 @@
-import EmailEditor, { EditorRef, EmailEditorProps } from 'react-email-editor';
-import {useRef} from 'react';
-import styled from 'styled-components';
+import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
+import { useRef } from "react";
+import styled from "styled-components";
 
 type iEmailTemplateBuilder = {
   designData: any;
   editorRef: (editor: EditorRef | null) => void;
-}
+};
 
 const Wrapper = styled.div``;
-const EmailTemplateBuilder = ({designData, editorRef}: iEmailTemplateBuilder) => {
+const EmailTemplateBuilder = ({
+  designData,
+  editorRef
+}: iEmailTemplateBuilder) => {
   const emailEditorRef = useRef<EditorRef | null>(null);
 
-  const onReady: EmailEditorProps['onReady'] = (unlayer) => {
+  const onReady: EmailEditorProps["onReady"] = unlayer => {
     // editor is ready
     // you can load your template here;
     // the design json can be obtained by calling
     // unlayer.loadDesign(callback) or unlayer.exportHtml(callback)
-
+    // emailEditorRef.current?.editor?.init({
+    //   customCSS: [
+    //     ".blockbuilder-branding {display: none !important;}"
+    //   ]
+    // })
     // const templateJson = { DESIGN JSON GOES HERE };
-    editorRef(emailEditorRef?.current)
+    editorRef(emailEditorRef?.current);
     if (Object.keys(designData).length > 0) {
       unlayer.loadDesign(designData);
     }
   };
 
-  return <Wrapper><EmailEditor ref={emailEditorRef} onReady={onReady} /></Wrapper>
-}
+  return (
+    <Wrapper className={"email-builder"}>
+      <EmailEditor
+        ref={emailEditorRef}
+        onReady={onReady}
+        minHeight={'1200px'}
+        options={{
+          safeHtml: true,
+          displayMode: 'email',
+        }}
+      />
+    </Wrapper>
+  );
+};
 
 export default EmailTemplateBuilder;
