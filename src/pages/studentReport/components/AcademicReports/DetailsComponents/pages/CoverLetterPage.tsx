eabc206890da1ee93160ad7feaedb4eaf2afa72c
Lin He <showmine.sysbox@gmail.com>
2024-06-14 13:33:32 +1000

    upgrade-packages: upgraded node version to 21

--- src/pages/studentReport/components/AcademicReports/DetailsComponents/pages/CoverLetterPage.tsx
+++ src/pages/studentReport/components/AcademicReports/DetailsComponents/pages/CoverLetterPage.tsx
@@ -26,15 +26,34 @@ const Wrapper = styled.div`
 
 const CoverLetterPage = ({student, studentReportYear, studentReportResult}: StudentAcademicReportDetailsProps & {studentReportResult: iStudentReportResult | null}) => {
 
+  const getImage = () => {
+    return (
+      <Image
+        src={student.profileUrl}
+        rounded
+        className={'pull-right'}
+        style={{padding: '1.4rem', width: '15rem', height: 'auto'}}
+      />
+    )
+  }
+
   const getCoverLetterContent = () => {
     if (`${studentReportYear.HideResultsToIds || ""}`
       .trim()
       .split(",")
       .map(id => Number(`${id || ""}`.trim()))
       .indexOf(student.StudentID) >= 0) {
-      return <div className={'p-3'}><Alert variant={'warning'}>Academic result has been marked as hidden. Further enquiries please contact school.</Alert></div>
+      return <FlexContainer className={'p-3 gap-2 align-items-start justify-content-between flex-wrap'}>
+        <Alert variant={'warning'}>Academic result has been marked as hidden. Further enquiries please contact school.</Alert>
+        {getImage()}
+      </FlexContainer>
     }
-    return <div dangerouslySetInnerHTML={{__html: studentReportYear.LetterOfExplanation || ''}}/>
+    return (
+      <>
+        {getImage()}
+        <div dangerouslySetInnerHTML={{__html: studentReportYear.LetterOfExplanation || ''}}/>
+      </>
+    )
   }
 
   return (
@@ -52,12 +71,7 @@ const CoverLetterPage = ({student, studentReportYear, studentReportResult}: Stud
       <PageTitleDivider student={student} studentReportResult={studentReportResult} />
 
       <SectionDiv className={'cover-letter'}>
-        <Image
-          src={student.profileUrl}
-          rounded
-          className={'pull-right'}
-          style={{padding: '1.4rem', width: '15rem', height: 'auto'}}
-        />
+
         {getCoverLetterContent()}
       </SectionDiv>
     </Wrapper>
