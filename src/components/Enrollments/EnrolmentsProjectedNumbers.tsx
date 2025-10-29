dec0997beb5e8ea78773f4043ccafc31b534ad4c
Lin He <lhe@mentonegirls.vic.edu.au>
2025-10-29 15:13:29 +1100

    master: added VRTQA.

--- src/components/Enrollments/EnrolmentsProjectedNumbers.tsx
+++ src/components/Enrollments/EnrolmentsProjectedNumbers.tsx
@@ -3,7 +3,7 @@ import moment from 'moment-timezone';
 import {useEffect, useState} from 'react';
 import SynVFutureStudentService from '../../services/Synergetic/SynVFutureStudentService';
 import MathHelper from '../../helper/MathHelper';
-import iSynVFutureStudent, {FUTURE_STUDENT_STATUS_FINALISED} from '../../types/Synergetic/iSynVFutureStudent';
+import {FUTURE_STUDENT_STATUS_FINALISED} from '../../types/Synergetic/iSynVFutureStudent';
 import Toaster from '../../services/Toaster';
 import { Spinner} from 'react-bootstrap';
 import FileYearSelector from '../student/FileYearSelector';
@@ -15,14 +15,18 @@ import Table, {iTableColumn} from '../common/Table';
 import SynLuFutureStatusService from '../../services/Synergetic/Lookup/SynLuFutureStatusService';
 import iSynLuFutureStatus from '../../types/Synergetic/Lookup/iSynLuFutureStatus';
 import SynVStudentService from '../../services/Synergetic/Student/SynVStudentService';
+import {MGG_CAMPUS_CODES} from '../../types/Synergetic/Lookup/iSynLuCampus';
+import iVStudent from '../../types/Synergetic/Student/iVStudent';
+import * as _ from 'lodash';
+import {OP_AND, OP_GTE, OP_LTE} from '../../helper/ServiceHelper';
 
-type iFutureEnrolmentsMap = {[key: number | string]: iSynVFutureStudent[]}
+type iFutureEnrolmentsMap = {[key: number | string]: iVStudent[]}
+type iFutureStatusMap = {[key: string]: iSynLuFutureStatus}
 const Wrapper = styled.div``;
 type iEnrolmentsProjectedNumbers = {
   className?: string;
   header?: React.ReactNode;
 }
-type iFutureStatusMap = {[key: string]: iSynLuFutureStatus}
 const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNumbers) => {
 
   const [isLoading, setIsLoading] = useState(true);
@@ -32,12 +36,15 @@ const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNum
   const [selectedYears, setSelectedYears] = useState<number[]>(defaultYears);
   const [selectedStatusCodes, setSelectedStatusCodes] = useState<string[]>([FUTURE_STUDENT_STATUS_FINALISED]);
   const [futureStatusMap, setFutureStatusMap] = useState<iFutureStatusMap>({});
+  const [currentStudents, setCurrentStudents] = useState<iVStudent[]>([]);
 
   useEffect(() => {
     if (selectedYears.length <=0 || selectedStatusCodes.length <= 0) {
       setFutureEnrolmentsMap({});
       return;
     }
+    const minSelectedYear = _.min(selectedYears);
+    const maxSelectedYear = _.max(selectedYears);
     let isCanceled = false;
     setIsLoading(true);
     Promise.all([
@@ -50,24 +57,31 @@ const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNum
         where: JSON.stringify({
           FutureEnrolYear: selectedYears,
           FutureStatus: selectedStatusCodes,
+          [OP_AND]: [
+            { FutureEnrolYear: {[OP_GTE]: minSelectedYear}},
+            { FutureEnrolYear: {[OP_LTE]: maxSelectedYear}},
+          ]
         }),
         perPage: 99999999,
       }),
       SynVStudentService.getCurrentVStudents({
         where: JSON.stringify({
           FileYear: currentYear,
-        })
+          StudentCampus: MGG_CAMPUS_CODES,
+          CurrentSemesterOnlyFlag: true,
+        }),
+        sort: 'FileSemester:DESC',
       })
     ])
       .then(([futureStatuses, futureStudentResult, currentStudentResult]) => {
         if (isCanceled) { return }
         const fStudentsMap = (futureStudentResult.data || []).reduce((map: iFutureEnrolmentsMap, fStudent) => ({
           ...map,
-          [`${fStudent.FutureEnrolYear}`]: [...(map[`${fStudent.FutureEnrolYear}`] || []), fStudent],
+          [`${fStudent.FutureEnrolYear}`]: [...(map[`${fStudent.FutureEnrolYear}`] || []), SynVFutureStudentService.mapFutureStudentToCurrent(fStudent)],
         }), {});
-        // const currentStudents = currentStudent || [];
         setFutureEnrolmentsMap(fStudentsMap);
-        setFutureStatusMap(futureStatuses.reduce((map: iFutureStatusMap, futureStatus) => ({ ...map, [futureStatus.Code]: futureStatus }), {}))
+        setFutureStatusMap(futureStatuses.reduce((map: iFutureStatusMap, futureStatus) => ({ ...map, [futureStatus.Code]: futureStatus }), {}));
+        setCurrentStudents(_.uniqBy(currentStudentResult, st => st.StudentID))
       })
       .catch(err => {
         if (isCanceled) { return }
@@ -81,7 +95,7 @@ const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNum
     return () => {
       isCanceled = true;
     }
-  }, [selectedYears, selectedStatusCodes]);
+  }, [selectedYears, selectedStatusCodes, currentYear]);
 
   const getHeader = () => {
     if (header) {
@@ -92,6 +106,97 @@ const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNum
         <FormLabel label={`All Future students with status`} />
       </FlexContainer>
     )
+  }
+
+  const getFutureTotal = (year: string  | number, statusCodes: string[] = []) => {
+    return (futureEnrolmentsMap[year] || [])
+      .filter(fStudent => statusCodes.length <= 0 ?  true : statusCodes.indexOf(fStudent.StudentStatus) >= 0)
+  }
+
+  const getCurrentStudents = (year: string | number) => {
+    return currentStudents.filter(student => {
+      const leavingDate = `${student.StudentLeavingDate || ''}`.trim();
+      if (leavingDate !== '' && moment(leavingDate).year() <= Number(currentYear)) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  const getContinuedStudents = (year: string | number) => {
+    const yearNumber = Number(year);
+    const yearDiff = MathHelper.sub(yearNumber, currentYear);
+    return _.uniqBy(getCurrentStudents(year)
+      // filter leaving date
+      .filter(student => {
+        const leavingDate = `${student.StudentLeavingDate || ''}`.trim();
+        if (leavingDate !== '' && Number(moment(leavingDate).year()) < Number(year)) {
+          return false;
+        }
+        return true;
+      })
+      // filter year level
+      .filter((student) => {
+        let yearLevel = Number(student.StudentYearLevel);
+        if (yearLevel === 30) {
+          yearLevel = -2;
+        } else if (yearLevel === 40) {
+          yearLevel = -1;
+        }
+        return MathHelper.add(yearLevel, yearDiff) <= 12;
+      }), st => st.StudentID);
+  }
+
+  const getCurrentStudentLeavingInThatYear = (year: string | number) => {
+    return getCurrentStudents(year)
+      // filter leaving date
+      .filter(student => {
+        const leavingDate = `${student.StudentLeavingDate || ''}`.trim();
+        if (leavingDate !== '' && `${moment(leavingDate).year() || ''}`.trim() === `${year || ''}`.trim()) {
+          return true;
+        }
+        return false;
+      })
+  }
+
+  const getCurrentStudentReturningInThatYear = (year: string | number) => {
+    return currentStudents
+      // filter leaving date
+      .filter(student => {
+        const returningDate = `${student.StudentReturningDate || ''}`.trim();
+        if (returningDate !== '' && `${moment(returningDate).year() || ''}`.trim() === `${year || ''}`.trim()) {
+          return true;
+        }
+        return false;
+      })
+  }
+
+  const getFinishingStudents = (year: string | number) => {
+    return getCurrentStudentLeavingInThatYear(year);
+  }
+
+  const getTotal = (year: string | number) => {
+    const continuedStudents = getContinuedStudents(year);
+    const previousYearFuture = getFutureTotal(MathHelper.sub(Number(year), 1));
+    const futureStudents = getFutureTotal(year);
+    const returningStudents = getCurrentStudentReturningInThatYear(year)
+      .filter(student => {
+        const leavingDate = `${student.StudentLeavingDate || ''}`.trim();
+        const returningDate = `${student.StudentReturningDate || ''}`.trim();
+        if (leavingDate !== '' && returningDate !== '' && moment(leavingDate) > moment(returningDate)) {
+          return false;
+        }
+        return true;
+      });
+    const finishingStudentIds = getFinishingStudents(year).map(student=> student.StudentID);
+
+    const totalStudents = [
+      ...(
+        [...continuedStudents, ...previousYearFuture, ...futureStudents].filter(st => finishingStudentIds.indexOf(st.StudentID) < 0)
+      ),
+      ...returningStudents
+    ];
+    return _.uniqBy(totalStudents, st => st.StudentID);
   }
 
   const getContent = () => {
@@ -130,29 +235,98 @@ const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNum
         <Table
           hover
           striped
-          rows={Object.keys(futureEnrolmentsMap)}
+          rows={selectedYears.map(year => `${year}`)}
           columns={[
             {
               key: 'year',
               header: '',
               cell: (col, data) => data,
             },
+            {
+              key: 'currentStudents',
+              header: 'Continued',
+              cell: (col, data) => {
+                const records = getContinuedStudents(data);
+                if (records.length <= 0) {
+                  return <td key={col.key}></td>;
+                }
+                return <td key={col.key}>
+                  <StudentNumberDetailsPopupBtn
+                    records={records}
+                    variant={'link'}>
+                    {records.length}
+                  </StudentNumberDetailsPopupBtn>
+                </td>
+              },
+            },
+            {
+              key: 'withdraw',
+              header: '- Withdraw',
+              cell: (col, data) => {
+                const records = getCurrentStudentLeavingInThatYear(data);
+                if (records.length <= 0) {
+                  return <td key={col.key}></td>;
+                }
+                return <td key={col.key}>
+                  <StudentNumberDetailsPopupBtn
+                    records={records}
+                    variant={'link'}>
+                    {records.length}
+                  </StudentNumberDetailsPopupBtn>
+                </td>
+              },
+            },
+            {
+              key: 'returning',
+              header: '+ Returning',
+              cell: (col, data) => {
+                const records = getCurrentStudentReturningInThatYear(data);
+                if (records.length <= 0) {
+                  return <td key={col.key}></td>;
+                }
+                return <td key={col.key}>
+                  <StudentNumberDetailsPopupBtn
+                    records={records}
+                    variant={'link'}>
+                    {records.length}
+                  </StudentNumberDetailsPopupBtn>
+                </td>
+              },
+            },
+            {
+              key: 'lastYearFuture',
+              header: (col: iTableColumn<string>) => <th key={col.key}>+ Future Prev Year</th>,
+              cell: (col, data) => {
+                const records = getFutureTotal(MathHelper.sub(Number(data), 1));
+
+                if (records.length <= 0) {
+                  return <td key={col.key}></td>;
+                }
+                return <td key={col.key}>
+                  <StudentNumberDetailsPopupBtn
+                    records={records}
+                    variant={'link'}>
+                    {records.length}
+                  </StudentNumberDetailsPopupBtn>
+                </td>
+              },
+            },
             ...((selectedStatusCodes || []).map(code => ({
               key: code,
-              header: code in futureStatusMap ? futureStatusMap[code].Description : code,
+              header: (col: iTableColumn<string>) => <th key={col.key}>+ {code in futureStatusMap ? futureStatusMap[code].Description : code}</th>,
               cell: (col: iTableColumn<string>, data: string) => {
-                const records = (futureEnrolmentsMap[data] || [])
-                  .filter(fStudent => fStudent.FutureStatus === code)
-                  .map(fStudent => SynVFutureStudentService.mapFutureStudentToCurrent(fStudent))
+                const records = getFutureTotal(data, [code]);
+                if (records.length <= 0) {
+                  return <td key={col.key}></td>;
+                }
                 return <td key={col.key}><StudentNumberDetailsPopupBtn records={records} variant={'link'}>{records.length}</StudentNumberDetailsPopupBtn></td>;
               }
             }))),
-            ...(selectedStatusCodes.length <= 1 ? [] : [{
+            {
               key: 'total',
-              header: () => <th className={'text-right'}>Total</th>,
+              header: (col: iTableColumn<string>) => <th className={'text-right'} key={col.key}>Total</th>,
               cell: (col: iTableColumn<string>, data: string) => {
-                const records = (futureEnrolmentsMap[data] || [])
-                  .map(fStudent => SynVFutureStudentService.mapFutureStudentToCurrent(fStudent))
+                const records = getTotal(data);
                 return (
                   <td key={col.key} className={'text-right'}>
                     <StudentNumberDetailsPopupBtn
@@ -163,7 +337,7 @@ const EnrolmentsProjectedNumbers = ({className, header}: iEnrolmentsProjectedNum
                   </td>
                 );
               }
-            }])
+            }
           ]}
         />
       </>
