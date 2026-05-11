dd84b1f2285e17709fa8e91b0ca6bd744a70f75b
lin he <lin.he@sysbox.com.au>
2026-05-12 08:55:46 +1000

    master: deleted __mocks__

--- __mocks__/highcharts-react-official.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HighchartsReact = (props: any) => {
-  return <div data-testid="HighchartsReactTestId">{props?.containerProps?.children || null}</div>;
-};
-
-export default HighchartsReact;
--- __mocks__/react-table-sticky.ts
+++
@@ -1,1 +0,0 @@
-export const useSticky = jest.fn();
