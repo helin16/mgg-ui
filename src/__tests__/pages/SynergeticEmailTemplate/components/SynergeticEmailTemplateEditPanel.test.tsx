dd6c45692d3b6f189b77bec9d2bfab4a0a5463c7
lin he <lin.he@sysbox.com.au>
2026-06-10 12:55:05 +1000

    004-schoolbox-host-bypass: added.

--- src/__tests__/pages/SynergeticEmailTemplate/components/SynergeticEmailTemplateEditPanel.test.tsx
+++
@@ -1,8 +0,0 @@
-describe('SynergeticEmailTemplateEditPanel', () => {
-  test('loads module exports', () => {
-    const mod = require('../../../../pages/SynergeticEmailTemplate/components/SynergeticEmailTemplateEditPanel');
-    const exported = mod.default || mod;
-
-    expect(exported).toBeTruthy();
-  });
-});
